3ff4fd8df95ea838df456d36872036b53b14a3a2
Jasmine G. <97357122+its-jasmine@users.noreply.github.com>
2025-06-05 10:22:25 -0400

    init

---
+++ src/main/java/com/jasminegadelhak/groupexpensetracker/GroupExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jasminegadelhak.groupexpensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasminegadelhak/groupexpensetracker/GroupExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasminegadelhak.groupexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
