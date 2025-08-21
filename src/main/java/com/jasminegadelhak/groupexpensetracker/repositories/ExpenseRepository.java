88082c27713863cd6fa2edf8a546e3a100273df7
Jasmine Gad El Hak <itsjessyg@gmail.com>
2025-08-21 12:59:49 -0400

    New repository query to find expenses by member who paid for it

--- src/main/java/com/jasminegadelhak/groupexpensetracker/repositories/ExpenseRepository.java
+++ src/main/java/com/jasminegadelhak/groupexpensetracker/repositories/ExpenseRepository.java
@@ -1,6 +1,7 @@
 package com.jasminegadelhak.groupexpensetracker.repositories;
 
 import com.jasminegadelhak.groupexpensetracker.model.Expense;
+import com.jasminegadelhak.groupexpensetracker.model.Member;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,9 @@ import java.util.List;
 @Repository
 public interface ExpenseRepository extends CrudRepository<Expense, Long> {
     public List<Expense> findAll();
+
+
+    List<Expense> findByPaidBy(Member member);
     @Query("SELECT SUM(e.amount) FROM Expense e")
     public Float sumAllExpenses();
 }
