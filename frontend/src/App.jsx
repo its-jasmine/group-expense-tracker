cf541690c655dc6e37b7a24c4edd57d70a646742
Jasmine Gad El Hak <itsjessyg@gmail.com>
2025-09-27 00:21:55 -0400

    Propogating data from api call made in parent App component, to Header component.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
 import './index.css'
 import Header from './components/Header'
 import Main from './components/Main'
+import api from './api'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [summaryData, setSummaryData] = useState({})
+  useEffect(() => {
+    api.getSummary().then(response => {
+      console.log(response)
+      setSummaryData(response)
+    })
+  }, [])
 
   return (
     <>
-      <Header />
+      <Header 
+        memberCount={summaryData.hasOwnProperty('memberCount') ? summaryData.memberCount : 0}
+        expenseCount={summaryData.hasOwnProperty('expenseCount') ? summaryData.expenseCount : 0}
+        expenseSum={summaryData.hasOwnProperty('expenseSum') ? summaryData.expenseSum : 0}
+      />
       <Main />
     </>
   )
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,25 @@
-export default function Header() {
+export default function Header({memberCount, expenseCount, expenseSum}) {
+    // Create a NumberFormat object for CAD Dollars in English
+    const formatterCAD = new Intl.NumberFormat('en-US', { // TODO move to separate formatting helper file
+    style: 'currency',
+    currency: 'CAD',
+    });
+
     return (
         <header>
             <div className="header-content">
                 <div className="logo">Divido</div>
                 <div className="stats">
                     <div className="stat-item">
-                        <h3>5</h3>
+                        <h3>{memberCount}</h3>
                         <p>Members</p>
                     </div>
                     <div className="stat-item">
-                        <h3>$847.50</h3>
+                        <h3>{formatterCAD.format(expenseSum)}</h3>
                         <p>Total Expenses</p>
                     </div>
                     <div className="stat-item">
-                        <h3>12</h3>
+                        <h3>{expenseCount}</h3>
                         <p>Transactions</p>
                     </div>
                 </div>
